09e18621add129a76451ddbe1b71ce61692af151
Rafael Kohler <rafael.kohler@gmail.com>
2019-05-18 15:12:10 -0300

    Criando operação de instanciação

--- src/main/java/com/rafaelkohler/mc/ModelagemconceituralApplication.java
+++ src/main/java/com/rafaelkohler/mc/ModelagemconceituralApplication.java
@@ -1,13 +1,34 @@
 package com.rafaelkohler.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelkohler.mc.domain.Categoria;
+import com.rafaelkohler.mc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemconceituralApplication {
+public class ModelagemconceituralApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceituralApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
 }
