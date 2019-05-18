6b53f9aeca1a839d2440b009f51af63cf50f4e4d
Rafael Kohler <rafael.kohler@gmail.com>
2019-05-18 15:36:44 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/rafaelkohler/mc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.rafaelkohler.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelkohler.mc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+
+}
